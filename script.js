6882ba23e64d495a8e75537e3293a95a38c27b63
Jonathon Nordquist <jonathon.nordquist@gmail.com>
2017-01-11 14:17:41 -0600

    Refactor, create new function to handle building of channel info

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 const switchBaseURL = "https://wind-bow.gomix.me/twitch-api/";
 const channels = [
-	"test_channel", "freecodecamp", "lefrenchrestream"
+	"test_channel", "freecodecamp", "imaqtpie"
 ]
 
-function getTwitchApiCall(endPoint, params, cb) {
+function returnTwitchApiCall(endPoint, params) {
 	if(Object.keys(params).length > 0  && params.constructor === Object) {
 		endPoint += "?"
 		for(key in params) {
 			endPoint += `${key}=${params[key]}`
 		}
 	}
-	console.log(switchBaseURL + endPoint)
-	fetch(switchBaseURL + endPoint)
-		.then(function(blob) { return blob.json() })
-		.then(function(data) { cb(data); });
+	return fetch(switchBaseURL + endPoint);
 }
 
+function buildChannelInformation(chan) {
+	return returnTwitchApiCall('channels/' + chan, {})
+			.then(function(b) {return b.json()})
+			// .then(function(data){console.log(data); return data;})
+			.then(function(data) {
+				return returnTwitchApiCall('streams/' + chan, {})
+					.then(function(c) {return c.json()})
+					.then(function(d) { 
+						data.currentlyStreaming = d.stream != null ? true : false;
+						return data;
+					});
+			})
+			.then(function(dman) {
+				console.log(dman);
+				return(dman);
+			});
+}
+
+
 for(let i = 0; i < channels.length; i++) {
-	getTwitchApiCall("channels/" + channels[i],
-							{},
-							function(b) {
-								appendChannelItem(createChannelItem(b));
-							});
+	buildChannelInformation(channels[i]).then(function(dbag) {
+		appendChannelItem(createChannelItem(dbag));
+	})
 }
 
 function createChannelItem(args) {
 	let outputNode = document.createElement('li')
-	outputNode.innerHTML = `<div class="chan-icon"><img src="${args.logo}"></div>`;
+	let channelName = args.display_name;
+	outputNode.innerHTML = `<div class="chan-icon">
+								<img src="${args.logo}">
+								// <h3 chan-title>args.display_name</h3>
+							</div>`;
 	return outputNode;
 }
 
@@ -34,5 +52,3 @@ function appendChannelItem(item) {
 	let listNode = document.querySelector(".channels");
 	listNode.appendChild(item);
 }
-
-// appendChannelItem(createChannelItem("asdf"))
