a20045c5716bd45522576161084b722475c46fc9
Jonathon Nordquist <jonathon.nordquist@gmail.com>
2017-01-11 20:46:59 -0600

    Correct problem with working dataset not being properly accessable which was preventing currentlyStreaming value from being set

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const switchBaseURL = "https://wind-bow.gomix.me/twitch-api/";
 const channels = [
-	"test_channel", "freecodecamp", "imaqtpie"
+	"test_channel", "freecodecamp", "imaqtpie", "timthetatman"
 ]
 
 function returnTwitchApiCall(endPoint, params) {
@@ -17,11 +17,14 @@ function buildChannelInformation(chan) {
 	let workingChanData;
 	return returnTwitchApiCall('channels/' + chan, {})
 			.then(function(b) {
-				workingChanData = b.json();
-				return workingChanData;
+				return b.json();
 			})
-			.then(function() {
+			.then(function(data) {
+				workingChanData = data;
 				return returnTwitchApiCall('streams/' + chan, {});
+			})
+			.then(function(blob) {
+				return blob.json();
 			})
 			.then(function(data) {
 				workingChanData.currentlyStreaming = data.stream != null ? true : false;
@@ -38,12 +41,9 @@ function channelFactory(channels) {
 		chanPromiseList.push(buildChannelInformation(channels[i]));
 	}
 	Promise.all(chanPromiseList).then(values => {
-		console.table(values)
 		values.sort(function(a, b) { 
 			return a.display_name.toLowerCase() < b.display_name.toLowerCase() ? -1 : 1; })
-		console.table(values)
 		values.forEach(function(val) {
-			console.log(val);
 			appendChannelItem(createChannelItem(val));
 		});
 	})
@@ -54,13 +54,14 @@ function createChannelItem(args) {
 	let channelName = args.display_name;
 	let isStreaming = args.currentlyStreaming ? "Online" : "Offline";
 	let chanLogo = args.logo || "https://dummyimage.com/200x200/fff/000.png&text=03XF"
-	outputNode.innerHTML = `<a href="https://www.twitch.tv/${args.display_name}" target="_blank">
-								<div class="chan-container">
-									<img class="chan-item chan-logo" src="${chanLogo}">
-									<h3 class="chan-item chan-title">${args.display_name}</h3>
-									<h3 class="chan-item chan-status">${isStreaming}</h3>
-								</div>
-							</a>`;
+	outputNode.innerHTML = 
+			`<a href="https://www.twitch.tv/${args.display_name}" target="_blank" class="streaming-${isStreaming.toLowerCase()}">
+				<div class="chan-container">
+					<img class="chan-item chan-logo" src="${chanLogo}">
+					<h3 class="chan-item chan-title">${args.display_name}</h3>
+					<h3 class="chan-item chan-status">${isStreaming}</h3>
+				</div>
+			</a>`;
 	return outputNode;
 }
 
@@ -69,4 +70,4 @@ function appendChannelItem(item) {
 	listNode.appendChild(item);
 }
 
-console.log(channelFactory(channels));
+channelFactory(channels);
